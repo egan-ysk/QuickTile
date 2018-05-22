2ebbb0d782c669a6890eaa7c836f07ce3670bbe1
Egan <971346471@qq.com>
2018-05-22 11:01:55 +0800

    wechatscan

--- app/src/main/java/com/egan/quicktile/service/WechatScanService.kt
+++ app/src/main/java/com/egan/quicktile/service/WechatScanService.kt
@@ -4,11 +4,16 @@ import android.content.ComponentName
 import android.content.Intent
 import android.service.quicksettings.TileService
 
+/**
+ * 微信扫一扫
+ */
 class WechatScanService : TileService() {
     override fun onClick() {
         super.onClick()
 
-        val intent = Intent("com.tencent.mm.action.BIZSHORTCUT").setComponent(ComponentName("com.tencent.mm", "com.tencent.mm.ui.LauncherUI"))
+        val intent = Intent().setComponent(ComponentName("com.tencent.mm", "com.tencent.mm.ui.LauncherUI"))
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        intent.putExtra("LauncherUI.From.Scaner.Shortcut", true)
         startActivityAndCollapse(intent)
     }
 }
